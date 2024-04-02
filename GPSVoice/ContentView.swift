6628b564107b26896d7181e9c0ba6faa582cc12e
denislav-d <ddenislav333@gmail.com>
2024-04-02 21:28:36 +0200

    Initial Commit

---
+++ GPSVoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPSVoice
+//
+//  Created by Denislav Dimitrov on 2.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPSVoice/GPSVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSVoiceApp.swift
+//  GPSVoice
+//
+//  Created by Denislav Dimitrov on 2.04.24.
+//
+
+import SwiftUI
+
+@main
+struct GPSVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
