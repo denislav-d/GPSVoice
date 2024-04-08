b8783e130bcf2e98ead40e053842fdb62cdb928a
denislav-d <ddenislav333@gmail.com>
2024-04-08 18:25:19 +0200

    Tracking the user's distance travelled in meters

--- GPSVoice/MapView.swift
+++ GPSVoice/MapView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 import CoreLocation
 import MapKit
 
+class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
+    var mapView: MapView?
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let mapView = mapView,
+              let newLocation = locations.last else { return }
+        
+        if let last = mapView.lastLocation {
+            let distance = last.distance(from: newLocation)
+            mapView.traveledDistance += distance
+        }
+        
+        mapView.lastLocation = newLocation
+    }
+}
+
 struct MapView: View {
+    @Binding var traveledDistance: Double
+    @Binding var lastLocation: CLLocation?
+    
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    let locationManager = CLLocationManager()
+    let delegate = LocationManagerDelegate()
     
     var body: some View {
         Map(position: $position) {
@@ -21,11 +42,15 @@ struct MapView: View {
             MapPitchToggle()
         }
         .onAppear {
-            CLLocationManager().requestWhenInUseAuthorization()
+            delegate.mapView = self
+            locationManager.delegate = delegate
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
         }
     }
 }
 
-#Preview {
-    MapView()
-}
+//
+//#Preview {
+//    MapView()
+//}
--- GPSVoice/NavigatorView.swift
+++ GPSVoice/NavigatorView.swift
@@ -6,45 +6,40 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 
 struct NavigatorView: View {
     @State private var isVoiceEnabled = false
     @State private var traveledDistance: Double = 0
+    @State private var lastLocation: CLLocation?
     
     var body: some View {
         NavigationStack {
-            MapView()
-            
             VStack {
-                HStack{
-                    Text("Total Distance Traveled:")
-                    Spacer()
-                    withAnimation {
+                MapView(traveledDistance: $traveledDistance, lastLocation: $lastLocation)
+                
+                VStack {
+                    HStack{
+                        Text("Total Distance Traveled:")
+                        Spacer()
                         Text("\(String(format: "%.1f", traveledDistance)) meters")
-                            .animation(.default)
-                            .contentTransition(.numericText())
                             .bold()
                     }
-                   
-                }
-                .padding(.horizontal)
-                
-                Toggle("Enable Voice Feedback", isOn: $isVoiceEnabled)
                     .padding(.horizontal)
-                    .onChange(of: isVoiceEnabled) {
-                        if isVoiceEnabled {
-                           //
-                        } else {
-                            //
+                    
+                    Toggle("Enable Voice Feedback", isOn: $isVoiceEnabled)
+                        .padding(.horizontal)
+                        .onChange(of: isVoiceEnabled) {
+                            // Handle voice feedback enabling/disabling
                         }
-                    }
+                }
+                .padding(.vertical)
             }
-            .padding(.vertical)
         }
     }
 }
 
-#Preview {
-    NavigatorView()
-}
+//#Preview {
+//    NavigatorView()
+//}
